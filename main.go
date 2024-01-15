d48e85eeaa95750526a00544d608020ff957100a
prapatn <prapat.werapan@gmail.com>
2024-01-15 14:41:14 +0700

    ทดสอบการยิง api

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"time"
 
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		time.Sleep(time.Millisecond * 10)
+		return c.SendString("Hello world")
+	})
+
+	app.Listen(":8000")
 }
